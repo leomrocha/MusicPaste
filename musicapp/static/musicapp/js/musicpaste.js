0fffa0e3d760c966565ec1446b0347e013fb1496
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2014-08-04 20:58:49 +0200

    added javascript display message when parse error. TODO sanitize input in server side

--- musicapp/static/musicapp/js/musicpaste.js
+++ musicapp/static/musicapp/js/musicpaste.js
@@ -1,6 +1,13 @@
 var mainApp = angular.module("MusicPaste", ["ui.bootstrap"]);
 
 ////////////////////////////////////////////////////////////////////////////////
+//Avoid template collition between Django and AngularJS
+mainApp.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+});
+
+////////////////////////////////////////////////////////////////////////////////
 // DIRECTIVES
 ////////////////////////////////////////////////////////////////////////////////
 /**
@@ -59,6 +66,10 @@ mainApp.directive('vexchord', function($compile){
 mainApp.controller('vextabController', ['$scope', function($scope) {
     //console.log("paper starting");
     //console.log($scope);
+    
+    $scope.parseOK = false;
+    $scope.parseError = "All is GOOOOD";
+    
     $scope.vextabText = 
 "\n\
 tabstave notation=true \n\
@@ -248,6 +259,7 @@ mainApp.directive('vextabPaper', ['$compile', function($compile) {
         scope.artist = artist;
         scope.vextab = vextab;
         scope.player = player;
+        var parseOK = false;
         
         var vextabText;
         function updateTab() {
@@ -260,10 +272,24 @@ mainApp.directive('vextabPaper', ['$compile', function($compile) {
                 vextab.parse(vextabText);
                 artist.render(renderer);
                 //console.log("artist = ", artist);
+                //save parse status
+                parseOK = true;
+                scope.parseOK = parseOK;
+                scope.parseError = "parse OK";
             }
             catch (e) {
-                console.log("Error");
-                console.log(e);
+                //console.log("Error");
+                //console.log(e);
+                //indicate parsing failed
+                parseOK = false;
+                scope.parseOK = parseOK;
+                if (e.hasOwnProperty("message")){
+                    //console.log("getting the message: ", e["message"]);
+                    scope.parseError = e["message"];
+                }else{
+                    //console.log("writing no matter what");
+                    scope.parseError = "Unknown parse Error";
+                }
             }      
             $compile(canvas)(scope);
             element.append(canvas);
@@ -273,18 +299,6 @@ mainApp.directive('vextabPaper', ['$compile', function($compile) {
                 player.fullReset(); //this is what makes the repaint correct
                 playerCanvas = element.find(".vextab-player");
                 scoreCanvas =  element.find(".vex-canvas");
-                //console.log("canvas = ", scoreCanvas);
-                //console.log(scoreCanvas.get(0).offsetTop);
-                //console.log(scoreCanvas.get(0).offsetLeft);
-                //playerCanvas.css("position", "absolute")
-                //            .css("z-index", 10)
-                //            //.css("top", scoreCanvas.get(0).offsetTop)
-                //            //.css("left", scoreCanvas.get(0).offsetLeft)
-                //            .css("top", 45)
-                //            .css("left", 15)
-                //            .css
-                //            ;
-                //playerCanvas.width = ;
                 //update canvas height
                 //console.log(playerCanvas);
                 playerCanvas.height = scoreCanvas.get(0).height;
