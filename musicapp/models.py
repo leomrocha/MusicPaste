1511181648e100523c8161bc3f61cd03889a5801
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2014-08-24 16:06:59 +0200

    using traditional uuid instead of short one. Let's see if this works out

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
 #unique ID
-import shortuuid
+#TODO find why this module is failing ALL the time
+#import shortuuid
+#For the moment use normal uuid and see
+import uuid
 
 # Create your models here.
 
 class Sheet(models.Model):
     """
     """
-    suuid = models.CharField(max_length=64, default=shortuuid.ShortUUID().random(length=22), editable=False) #TODO fix the bug in shortuuid library
+    #suuid = models.CharField(max_length=64, default=shortuuid.ShortUUID().random(length=22), editable=False) #TODO fix the bug in shortuuid library
     #suuid = models.CharField(max_length=64, default=shortuuid.uuid(name="musicpaste.com"), editable=False) #NOP, this one worked WORST, EVERY single instance is equal
+    suuid = models.CharField(max_length=64, default=uuid.uuid4(), editable=False) #Let's try with traditional uuid and see
     title = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
