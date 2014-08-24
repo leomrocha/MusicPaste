18858e940d805f9f938f259cfe418c5e5aaa4864
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2014-08-24 16:21:45 +0200

    bug introduced because I'm too pressed with uuid

--- musicapp/models.py
+++ musicapp/models.py
@@ -13,7 +13,7 @@ class Sheet(models.Model):
     """
     #suuid = models.CharField(max_length=64, default=shortuuid.ShortUUID().random(length=22), editable=False) #TODO fix the bug in shortuuid library
     #suuid = models.CharField(max_length=64, default=shortuuid.uuid(name="musicpaste.com"), editable=False) #NOP, this one worked WORST, EVERY single instance is equal
-    suuid = models.CharField(max_length=64, default=uuid.uuid4().replace('-',''), editable=False) #Let's try with traditional uuid and see
+    suuid = models.CharField(max_length=64, default=str(uuid.uuid4()).replace('-',''), editable=False) #Let's try with traditional uuid and see
     title = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
