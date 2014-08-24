56d3d4194b1cbc2136afdb9ba06f388c33476cd7
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2014-08-24 16:34:31 +0200

    reverted to old library, because the problem doesn't come from the lib

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
 #unique ID
-#TODO find why this module is failing ALL the time
-#import shortuuid
+import shortuuid
 #For the moment use normal uuid and see
-import uuid
+#import uuid
 
 # Create your models here.
 
 class Sheet(models.Model):
     """
     """
-    #suuid = models.CharField(max_length=64, default=shortuuid.ShortUUID().random(length=22), editable=False) #TODO fix the bug in shortuuid library
-    #suuid = models.CharField(max_length=64, default=shortuuid.uuid(name="musicpaste.com"), editable=False) #NOP, this one worked WORST, EVERY single instance is equal
-    suuid = models.CharField(max_length=64, default=str(uuid.uuid4()).replace('-',''), editable=False) #Let's try with traditional uuid and see
+    suuid = models.CharField(max_length=64, default=shortuuid.ShortUUID().random(length=22), editable=False) 
+    #suuid = models.CharField(max_length=64, default=str(uuid.uuid4()).replace('-',''), editable=False)
     title = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
